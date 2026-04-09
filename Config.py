d00fdd8dc36f76bf38fe907dcdb31e83264df433
KSBOTSOFFICAL1 <littlesingham968@gmail.com>
2026-04-09 21:33:48 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,7 +3,7 @@
 import os
 
 # Bot token from @botfather
-BOT_TOKEN = "8532720894:AAGh3nbCNanOoTeCbvkwKYb1AGmjFr4oLRM"
+BOT_TOKEN = "8619959255:AAGEkg8chHCcgaRkFVYcJ10mrFqyyNq2Y9o"
 # From my.telegram.org/
 API_ID = "29481626"
 API_HASH =  "4892185769903521077c4cea97808b8c"
@@ -13,5 +13,5 @@ OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "8532720894").split("8532
 AUTH_USERS = [8532720894]
 # Time gap after each request (in seconds) 
 TIME_GAP = 1
-BOT_USERNAME='animeCartoonLive_bot' #bot username without@
-API_TOKEN='65aa5be4d757fb7242fff9dde00f6cd5d4acc977' #don't change this without developer permission
+BOT_USERNAME='M3u8LiveRecordingBot' #bot username without@
+API_TOKEN='9503d9bf87c90aa9e0aab35d4dec7d1ce24c0a23' #don't change this without developer permission
